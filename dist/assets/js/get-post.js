15c8d0f385053a7b5a12cc9246866c023ace8a88
yeonoh <beky010101@gmail.com>
2023-08-31 14:50:06 +0900

    Feat: 오운완 전체 조회 카드 오류 해결

--- dist/assets/js/get-post.js
+++ dist/assets/js/get-post.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 });
 
 const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjkzNDUwMjkxLCJleHAiOjE2OTM0NTc0OTF9.5D-ZVt2YclaCazKDwN59QvyCdJDPcALUzWUjD9ktk30';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjkzNDU4MjA4LCJleHAiOjE2OTM0NjU0MDh9.AuAKkmvgbKLXox-LbIMoIuJSgZ7HVKqiI9iHQ69COHs';
 
 // 오운완 전체 조회
 // challengeId 받아오는거 아직 안함.
@@ -17,6 +17,7 @@ const getPosts = async () => {
         },
       },
     );
+    console.log(response.data);
 
     let allPosts = '';
     response.data.data.forEach((post) => {
@@ -25,30 +26,29 @@ const getPosts = async () => {
       }
 
       let temphtml = `<div class="col-12 col-md-4 col-lg-4">
-                              <article class="article article-style-c">
-                                  <div class="article-header">
-                                      <div class="article-image" data-background="${post.imgUrl}">
-                                      </div>
+                        <article class="article article-style-c">
+                          <div class="article-header">
+                              <div class="article-image" data-background="${post.imgUrl}">
+                                </div>
                                   </div>
                                   <div class="article-details">
-                                      
                                       <div class="article-title">
                                           <h2><a href="#">${post.description}</a></h2>
                                       </div>
                                       <div class="article-user">
                                           <img alt="image" src="assets/img/avatar/avatar-3.png">
                                           <div class="article-user-details">
-                                              <div class="user-detail-name">
-                                                  <a href="#">${post.username}</a>
-                                              </div>
-                                              <div class="text-job">${post.comment}</div>
-                                          </div>
+                                            <div class="user-detail-name">
+                                                <a href="#">${post.username}</a>
+                                            </div>
+                                          <div class="text-job">${post.comment}</div>
                                       </div>
                                   </div>
-                              </article>
-                          </div>`;
+                              </div>
+                          </article>
+                      </div>`;
       allPosts += temphtml;
-      $('.card').html(allPosts);
+      $('.row').html(allPosts);
     });
   } catch (error) {
     console.error('Error message:', error.response.data.message);
